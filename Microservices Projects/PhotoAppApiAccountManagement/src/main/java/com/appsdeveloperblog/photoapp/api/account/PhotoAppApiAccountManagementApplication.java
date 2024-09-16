e15318e109637a3267a848cd78b44cd82547c74f
PavanKalyan <pavankalyan6md@gmail.com>
2024-09-16 10:37:13 +0530

    Commit1

--- Microservices Projects/PhotoAppApiAccountManagement/src/main/java/com/appsdeveloperblog/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
+++ Microservices Projects/PhotoAppApiAccountManagement/src/main/java/com/appsdeveloperblog/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
@@ -9,5 +9,4 @@ public class PhotoAppApiAccountManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
 	}
-
 }
